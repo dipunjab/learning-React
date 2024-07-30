601bb6ea958e51b62bf4ec2ad36a64537ff124a3
Usman <ghaniusman0287@gmail.com>
2024-07-30 07:54:20 +0500

    setting up react in 2 wayss

---
+++ 01bascireact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+      <h1>Chai aur React | Usman Ghani</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>ViteReact | Usman Ghani</h1>
+  )
+}
+
+export default App
